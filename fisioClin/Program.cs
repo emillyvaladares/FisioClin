006039683e7a901a09a05da33729c86706daf8b8
biancancmnt <160256079+biancancmnt@users.noreply.github.com>
2025-10-10 16:16:30 -0400

    conserto do erro no bd e na página cadastrar paciente

--- fisioClin/Program.cs
+++ fisioClin/Program.cs
@@ -1,4 +1,6 @@
 using fisioClin.Components;
+using fisioClin.Configs;
+using fisioClin.Models; // <-- certifique-se de importar o namespace que contém Conexao e PacienteDAO
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,18 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// ?? REGISTRA AS DEPENDÊNCIAS
+builder.Services.AddScoped<Conexao>();
+builder.Services.AddScoped<PacienteDAO>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
