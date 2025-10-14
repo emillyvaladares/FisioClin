bd090f98ce6fa99a4b88541c16b11d7ed55adedf
emillyvaladares <valadaresemillylavinia@gmail.com>
2025-10-14 16:16:47 -0400

    mudanca de banco

--- fisioClin/Models/bd_fisioClin.sql
+++ fisioClin/Models/bd_fisioClin.sql
@@ -1,51 +1,38 @@
 create database FisioClin;
 use FisioClin;
 
-create table Endereco (
-id_end int primary key auto_increment,
-cep_end varchar (20),
-bairro_end varchar (200),
-rua_end varchar (200),
-numero_end varchar (100)
-);
-
 create table Paciente (
 id_paciente int primary key auto_increment,
 nome_pac varchar (200),
 cpf_pac varchar (15),
+cep_pac varchar (20),
 rg_pac varchar (20),
+bairro_pac varchar (200),
 data_nascmento_pac date,
+rua_pac varchar (200),
+numero_pac varchar (100),
 sexo_pac varchar (10),
 email_pac varchar (200),
-telefone_pac varchar (20),
-id_end_fk  int not null, 
-foreign key (id_end_fk) references endereco(id_end)
+telefone_pac varchar (20)
 );
 
+insert into Paciente values (1,'emilly', '060.042.332-21', '789598', '375723',
+ 'bairro parque São Pedro', '2007-05-31', 'Rua Tupã', '145', 'feminino', 
+ 'valadaresemillylavinia@gmail.com', '69 99275-1820');
+ 
 create table Laudo (
 id_laudo int primary key auto_increment,
-tipo_exame varchar (200),
+Validade_laudo varchar (200),
+Tipo_Exame varchar (200),
 diagnostico_laudo varchar (200),
 observacao_laudo varchar (200),
+status_laudo varchar (200),
 id_paciente_fk int not null,
 foreign key (id_paciente_fk) references Paciente(id_paciente)
 );
 
-create table Funcionarios (
-id_funcionario int primary key auto_increment,
-nome_func varchar (200),
-cpf_func varchar (15),
-rg_func varchar (20),
-email_func varchar (200),
-data_nascmento_func date,
-especialidade_func varchar(200),
-registro_profissional_func varchar (200),
-data_contratacao_func date,
-tipo_vinculo_func varchar (200),
-certificados_func varchar (200),
-telefone_pac varchar (20),
-senha_func varchar (200)
-);
+insert into Laudo Values (1, 'Valido por 1 ano', 'Queropraxia', 'Escoliose', 'Paciente sente muitas dores',
+'Em aberto',1);
 
 create table Cargo (
 id_cargo int primary key auto_increment,
@@ -55,19 +42,46 @@ descricao_cargo varchar (200),
 carga_horaria int,
 data_criacao date,
 data_atualizacao date,
-observacoes_cargo varchar (200),
-id_funcionario_fk int not null,
-foreign key (id_funcionario_fk) references Funcionarios(id_funcionario)
+observacoes_cargo varchar (200)
 );
+
+insert into Cargo values (1, 'Queroprata', 'Fisioterapia',
+ 'Realiza trabalho com pessoas com problema nas articulações.', 8, '2018-07-15', '2023-04-26',
+ 'Importante');
+
+create table Funcionarios (
+id_funcionario int primary key auto_increment,
+nome_func varchar (200),
+cpf_func varchar (15),
+tipo_vinculo varchar (200),
+senha_func varchar (200),
+rg_func varchar (20),
+especialidade_func varchar (200),
+subespecialidade_func varchar (200),
+telefone_pac varchar (20),
+email_func varchar (200),
+registro_profissional_func varchar (200),
+certificados_func varchar (200),
+data_nascmento_func date,
+data_contratacao_func date
+);
+
+Insert into Funcionarios values (1, 'Renata Chagas', '032.854.754-44', 'clt',
+'emilly123', '737564', 'Queropatra', 'Pediatra', '69 99535-3466', 'jhdsfbsdj@gmail.com'
+, '35734', 'Curso De Queropraxia', '2000-08-23', '2024-05-27');
 
 create table Sala (
 id_sala int primary key auto_increment,
+nome_sala varchar (200),
 numero_sala varchar (200),
-tipo_sala varchar (200),
-observacao_sala varchar (200),
 capacidade_sala varchar (200),
-disponibilidade_sala varchar (50)
+tipo_sala varchar (200),
+disponibilidade_sala varchar (50),
+observacao_sala varchar (200)
 );
+
+insert into Sala values (1, 'Sala de Massagem', '23', '50 pessoas', 'Sala para relaxamento',
+'Ocupada', 'Sala com equipamentos para a realização de massagens.');
 
 create table Sessao (
 id_sessao int primary key auto_increment,
@@ -80,6 +94,9 @@ foreign key (id_funcionario_fk) references Funcionarios(id_funcionario),
 id_paciente_fk int not null,
 foreign key (id_paciente_fk) references Paciente(id_paciente)
 );
+
+insert into Sessao values (1, '2025-12-05', '10:00', 'Sessao Ortopédica',
+ 'sessao para paciente compé torcido', 1, 1);
 
 create table Agenda (
 id_agenda int primary key auto_increment,
@@ -96,6 +113,8 @@ foreign key (id_paciente_fk) references Paciente(id_paciente),
 observacao_ag varchar (200)
 );
 
+Insert into Agenda values (1, '2025-10-23', '09:00', 1, 1, 1, 1, 'Agenda do dia 23');
+
 create table Prontuario (
 id_prontuario int primary key auto_increment,
 data_pront date,
@@ -105,10 +124,12 @@ doenca_previa_pront varchar (200),
 historico_familiar_pront varchar (200),
 habito_vida_pront varchar (200),
 avaliacao_pront varchar(200),
-status_pront varchar(200),
 id_paciente_fk int not null,
 foreign key (id_paciente_fk) references Paciente(id_paciente)
 );
+
+iNSERT Into Prontuario values (1, '2025-03-28', 'dipirona', null, null, 
+'diabetes', 'Caminhadas', 'Péssimo estado', 1); 
 
 create table Pagamento (
 id_pagamento int primary key auto_increment,
@@ -117,8 +138,13 @@ numero_parcelas int,
 valor_pagamento float,
 forma_pagamento varchar (200),
 status_pagamento varchar (200),
-observacao_pagamento varchar (200)
+observacao_pagamento varchar (200),
+id_paciente_fk int not null,
+foreign key (id_paciente_fk) references Paciente(id_paciente)
 );
+
+Insert into Pagamento values (1, '2025-09-15', 5, 170, 'crédito', 'Em processo, duas parcelas pagas',
+'Paciente tem pago corretamente', 1);
 
 create table financeiro ( 
 id_financeiro int primary key auto_increment,
@@ -126,3 +152,5 @@ periodo_fin float,
 id_funcionario_fk int not null,
 foreign key (id_funcionario_fk) references Funcionarios(id_funcionario)
 );
+
+Insert into Financeiro values (1, 12, 1);
