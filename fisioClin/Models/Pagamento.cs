2699f8c903d7e2d646af22145b5d8da868ad1189
Roberta Castellan <robertacastellanr5@gmail.com>
2025-10-15 17:44:04 -0300

    Pagamento

--- fisioClin/Models/Pagamento.cs
+++ fisioClin/Models/Pagamento.cs
@@ -2,5 +2,20 @@
 {
     public class Pagamento
     {
+        public int Id { get; set; }
+
+        public string DataPagamento { get; set; }
+
+        public string NumeroParcelas { get; set; }
+
+        public string ValorPagamento { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public string Status { get; set; }
+
+        public string Observacao { get; set; }
+
+        public int IdPacienteFk { get; set; }  // FK para o paciente
     }
 }
