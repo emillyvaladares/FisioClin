05d40602cad610e4f8750feeb6225be2ffdae4e3
emillyvaladares <valadaresemillylavinia@gmail.com>
2025-10-16 11:08:24 -0400

    agenda e login

--- fisioClin/Models/Login.cs
+++ fisioClin/Models/Login.cs
@@ -2,5 +2,10 @@
 {
     public class Login
     {
+        public int Id { get; set; }
+
+        public int Id_Paciente_fk { get; set; }
+        
+        public int Id_Funcionario_fk { get; set; }
     }
 }
